123938208c0c7d4cdef3e90978a5577241af589a
oliveeyay <olivier.goutay@gmail.com>
2023-12-15 10:27:52 +0000

    Setting up a multiplatform Swift project

---
+++ SharedModule/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftWasmer
+//
+//  Created by Olivier Goutay on 15/12/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SharedModule/SwiftWasmerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftWasmerApp.swift
+//  SwiftWasmer
+//
+//  Created by Olivier Goutay on 15/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftWasmerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
